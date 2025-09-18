3f7553f4bd1f9675cb413348b39b589505a8c062
shimfff <shimseawon1510@gmail.com>
2025-09-18 14:41:55 +0900

    [feat] 여러개 커넥터 등록을 위해 커넥터 등록 함수 생성

--- scripts/register-connector.sh
+++ scripts/register-connector.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
-CONNECTOR_CONFIG_FILE=$1
-CONNECTOR_NAME=$(jq -r '.name' "$CONNECTOR_CONFIG_FILE")
 
-CONNECT_URL="http://localhost:8083" # todo 배포용으로 수정 필요
+CONNECT_URL="http://connect:8083" # todo 배포용으로 수정 필요, env로 받기
+
+# 커넥터 등록 함수
+register_connector() {
+  CONFIG_FILE=$1 # 커넥터 설정 파일 경로
+  CONNECTOR_NAME=$(jq -r '.name' "$CONFIG_FILE") # 커넥터 이름 추출
+
+  echo "Registering connector '$CONNECTOR_NAME'..."
+
+  TMP_FILE=/tmp/connector-config-resolved.json # 임시 파일 경로
+  envsubst < "$CONFIG_FILE" > $TMP_FILE # 환경 변수 치환
+
+  echo "=== Using resolved connector config for $CONNECTOR_NAME ==="
+  cat $TMP_FILE
+
+  # 커넥터 존재 여부 확인 후 등록
+  STATUS=$(curl -s -o /dev/null -w "%{http_code}" $CONNECT_URL/connectors/$CONNECTOR_NAME)
+  if [ "$STATUS" == "200" ]; then
+    echo "Connector '$CONNECTOR_NAME' already exists. Skipping..."
+  else
+    curl -s -X POST -H "Content-Type: application/json" \
+      --data @"$TMP_FILE" \
+      $CONNECT_URL/connectors
+    echo "Connector '$CONNECTOR_NAME' registration sent."
+  fi
+}
 
 # REST API 준비될 때까지 대기
 while true; do
   HTTP_CODE=$(curl -s -o /dev/null -w %{http_code} $CONNECT_URL/)
   if [ "$HTTP_CODE" == "200" ]; then
-    echo "Kafka Connect REST API ready. Registering connector '$CONNECTOR_NAME'..."
+    echo "Kafka Connect REST API ready."
     break
   fi
   echo "Waiting for Kafka Connect REST API (Status: $HTTP_CODE)..."
   sleep 5
 done
 
-# 치환된 JSON을 임시 파일로 저장
-TMP_FILE=/tmp/connector-config-resolved.json
-envsubst < "$CONNECTOR_CONFIG_FILE" > $TMP_FILE
-
-echo "=== Using resolved connector config ==="
-cat $TMP_FILE
-
-# 커넥터 등록
-curl -X POST -H "Content-Type: application/json" \
-     --data @"$TMP_FILE" \
-     $CONNECT_URL/connectors
-
-echo "Connector registration sent."
+# 여러 커넥터 등록
+register_connector /etc/kafka-connect/connector-config.json
+register_connector /etc/kafka-connect/mongo-sink-config.json
